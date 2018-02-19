1e63aec245a44ec8eebcba0d24ab617f15555b75
John Steill <jsteill@morgridge.org>
2018-02-18 18:31:33 -0600

    need_to_debug_log

--- HW1/learn_motif.py
+++ HW1/learn_motif.py
@@ -1,75 +1,114 @@
 import argparse, os, sys
 import numpy as np
 import itertools
+from scipy.special import logsumexp
 
 PI = .7
+CHAR_MP = {i:el for el,i in enumerate('ACGT')} 
+L = 0
+n = 0
+W = 0
+
 
 # You can choose to write classes in other python files
 # and import them here.
 
-def  E_step_get_Z(pwm, sequences):
-    pass
+def  E_step_get_Z(pwm, Z, sequences):
+    global CHAR_MP, L, n, W
+    pwm = np.log(pwm)
+    for i,seq in enumerate(sequences):
+        for j in range(L-W+1):
+            motif_range = range(j,j+W)
+            non_motif_range = itertools.chain(range(j),range(j+W,L))
+            Z[i,j] = 0
+            for k in non_motif_range:
+                Z[i,j] += pwm[CHAR_MP[seq[k]],0]
+            for k in motif_range:
+                Z[i,j] += pwm[CHAR_MP[seq[k]],k-j+1]
+    log_prob = sum(np.apply_along_axis(logsumexp,axis=1,arr=Z))
+    z_nrm = np.exp(np.apply_along_axis(logsumexp,axis=1,arr=np.log(Z)))
+    Z = np.exp(Z) / np.exp
+    Z /= Z.sum(axis=1)[:, np.newaxis]
+    return (Z, log_prob)
+                
+def M_step_get_pwm(pwm, Z,sequences):
+    global CHAR_MP, L, n, W
+    pwm = np.ones((4,W+1))
+    for i,seq in enumerate(sequences):
+        for j in range(L-W+1):
+            motif_range = range(j,j+W)
+            non_motif_range = itertools.chain(range(j),range(j+W,L))
+            for k in non_motif_range:
+                pwm[CHAR_MP[seq[k]],0] += Z[i,j]
+            for k in motif_range:
+                pwm[CHAR_MP[seq[k]],k-j+1] += Z[i,j]
+    pwm /= pwm.sum(axis=0)
 
-def M_step_get_pwm(z,sequences):
-    pass
-
-def get_log_prob(pwm,z,sequences):
-    pass
 
 def load_sequences(f):
-    pass
+    global CHAR_MP, L, n, W
+    with open(f,'r') as seq_file:
+        return [el.strip() for el in seq_file.readlines()]
 
 
-def get_init_motif(W,pi,load_sequences):
-    init_log_prob = -999999999.
+def get_init_motif(sequences):
+    global CHAR_MP, L, n, W
+    best_prob = -999999999.
     background = .25 * np.ones((4,1));
-    init_probs = { el: (1-pi)/3 * np.ones((4,1)) for el in 'ACGT'}
-    for i,z in enumerate(init_probs): 
-        init_probs[z][i] = pi
+    init_probs = { el: (1-PI)/3 * np.ones((4,1)) for el in 'ACGT'}
+    for i,ch in enumerate(init_probs): 
+        init_probs[ch][i] = PI
     def get_pwm_from_motif(motif):
-        return np.hstack(((background,) + tuple(init_probs[z] for z in motif)))
+        return np.hstack(((background,) 
+            + tuple(init_probs[ch] for ch in motif)))
     def get_k_mers_of_str(s,k):
         return list(s[i:i+k] for i in range(len(s)-k+1))
     def get_all_kmers(seqs,k):
-        return (el for el in set(sum([get_k_mers_of_str(s,k) for s in seqs]))) 
+        return list(set(itertools.chain(
+            *[get_k_mers_of_str(s,k) for s in seqs])))
+
 
     L = len(sequences[0])
+    n = len(sequences)
     len_Z = L - W + 1
-    Z = np.ones(n,len_Z) / len_Z
+    Z = np.ones((n,len_Z)) / len_Z
     for motif in get_all_kmers(sequences,W):
         pwm = get_pwm_from_motif(motif)
-        z = E_step_get_Z(pwm, sequences)
-        pwm = M_step_get_pwm(z,sequences)
-        prob = get_log_prob(pwm,z,sequences)
+        Z, prob = E_step_get_Z(pwm, Z, sequences)
         if prob > best_prob:
+            pwm = M_step_get_pwm(pwm, Z,sequences)
             best_prob = prob
-            best_pwm = pwm
-            best_z = z
-        pwm = best_pwm
-        z = best_z
-     return (pwm, z)  
+            print(best_prob)
+            best_Z = Z
+    Z = best_Z
+    return (pwm, Z)  
 
-def write_output(pwm,z,sequences,args):
+def write_output(pwm,Z,sequences,args):
+    global CHAR_MP, L, n, W
     pass
 
 
 def main(args):
-    model_file_path = args.M
-    position_file_path = args.P
-    subseq_file_path = args.SUBSEQ
+    global CHAR_MP, L, n, W
+    W = args.W
 
     sequences = load_sequences(args.SEQ)
-    (pwm, z) = get_init_motif(args.W, PI, sequences)
+    (pwm, Z) = get_init_motif(sequences)
+
+    print(pwm, Z)
     
     pwm_old = pwm
     converged = False
     while not converged:
-        pwm = get_pwm_from_motif(z, sequences)
-        z = E_step_get_Z(pwm, sequences)
+        pwm = get_pwm_from_motif(Z, sequences)
+        Z = E_step_get_Z(pwm, sequences)
         converged = np.linalg.norm(pwm-pwm_old) < 1e-10
         pwm_old = pwm
 
-    write_output(pwm, z, sequences, args)
+    write_output(pwm, Z, sequences, args)
+    model_file_path = args.M
+    position_file_path = args.P
+    subseq_file_path = args.SUBSEQ
 
 
 # Note: this syntax checks if the Python file is being run as the main program
